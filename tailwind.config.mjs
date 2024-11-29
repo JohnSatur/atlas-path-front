32ba21fa1371474abe85a9cf4bb6475609114df2
John Satur <animashorse3@yahoo.com.mx>
2024-11-29 17:22:52 -0600

    Cambios en hero section de home

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,27 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				brand: {
+					DEFAULT: '#1C84B6',
+					50: '#e8f2f8',
+          100: '#c9e0f2',
+          200: '#a9cdf6',
+          300: '#89bae0',
+          400: '#698eb0',
+          500: '#1c84b6',
+          600: '#186ca0',
+          700: '#14548a',
+          800: '#0f3974',
+          900: '#0a235e',
+				},
+			},
+			fontFamily: {
+				title: ['GFS Didot', 'serif'],
+				body: ['GFS Neohellenic', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [],
 }
