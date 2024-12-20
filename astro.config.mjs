c4f40a6f66346552e7ea9a8c864b4c675f13d616
John Satur <animashorse3@yahoo.com.mx>
2024-12-20 16:19:30 -0600

    Desintegración con mdx (talvez algun bug)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +10,5 @@ export default defineConfig({
   integrations: [
     tailwind(),
     sitemap(),
-    mdx()
   ]
 });
