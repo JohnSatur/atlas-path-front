642ed427d25be374f2ad584f879cb8d81fd3901f
John Satur <animashorse3@yahoo.com.mx>
2025-01-02 13:46:29 -0600

    Probando funcionalidad del adaptador de Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://therealatlaspath.com',
+
   integrations: [
     tailwind(),
     sitemap(),
-  ]
+  ],
+
+  adapter: netlify()
 });
