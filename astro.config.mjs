bb28a4c2756f00bff2c98002954ae202fb4aa0f0
John Satur <animashorse3@yahoo.com.mx>
2025-06-25 21:03:31 -0600

    Eliminación del adaptador de Netlify y ajustes en la configuración de Astro. Actualización de estadísticas en la sección de estadísticas y adición de la política de privacidad en una nueva página.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
-import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +10,5 @@ export default defineConfig({
   integrations: [
     tailwind(),
     sitemap(),
-  ],
-
-  adapter: netlify()
+  ]
 });
